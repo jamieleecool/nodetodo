6d886054566fe7cef0c7ce0b65b30fbe8ee56ddf
Jamie Cool <jamiec@microsoft.com>
2016-09-30 23:30:18 -0400

    fixup the script

--- deploy.sh
+++ deploy.sh
@@ -11,11 +11,11 @@ configure_aws_cli(){
 
 deploy_cluster() {
 
-    family="sample-webapp-task-family"
+    family="nodetodotask-circle"
 
     make_task_def
     register_definition
-    if [[ $(aws ecs update-service --cluster sample-webapp-cluster --service sample-webapp-service --task-definition $revision | \
+    if [[ $(aws ecs update-service --cluster Prod --service WebTier --task-definition $revision | \
                    $JQ '.service.taskDefinition') != $revision ]]; then
         echo "Error updating service."
         return 1
@@ -24,7 +24,7 @@ deploy_cluster() {
     # wait for older revisions to disappear
     # not really necessary, but nice for demos
     for attempt in {1..30}; do
-        if stale=$(aws ecs describe-services --cluster sample-webapp-cluster --services sample-webapp-service | \
+        if stale=$(aws ecs describe-services --cluster Prod --services WebTier | \
                        $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
             echo "Waiting for stale deployments:"
             echo "$stale"
@@ -41,14 +41,14 @@ deploy_cluster() {
 make_task_def(){
 	task_template='[
 		{
-			"name": "go-sample-webapp",
-			"image": "%s.dkr.ecr.us-east-1.amazonaws.com/go-sample-webapp:%s",
+			"name": "nodetodotask",
+			"image": "%s.dkr.ecr.us-east-1.amazonaws.com/nodetodo:%s",
 			"essential": true,
-			"memory": 200,
-			"cpu": 10,
+			"memory": 1000,
+			"cpu": 200,
 			"portMappings": [
 				{
-					"containerPort": 8080,
+					"containerPort": 3000,
 					"hostPort": 80
 				}
 			]
