8dc1a7babbbad9259e31ef60760bf8fc2604d919
Jamie Cool <jamiec@microsoft.com>
2016-10-03 13:05:45 -0400

    fix css

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './app/containers/App';
 import configureStore from './app/store/configureStore';
 import {Router, Route, browserHistory} from 'react-router';
 
-import 'todomvc-app-css/index.css';
+import '.app/index.scss';
 
 const store = configureStore();
 
